c280e3b399c757a63df459542023e1c3736fc267
GhaithBrunel <2157226@brunel.ac.uk>
2023-10-27 19:33:18 +0100

    added sound effects

--- Platformer/Assets/Scripts/ItemCollector.cs
+++ Platformer/Assets/Scripts/ItemCollector.cs
@@ -7,11 +7,12 @@ public class ItemCollector : MonoBehaviour
 {
     private int kiwis = 0;
     [SerializeField] private TextMeshProUGUI kiwisText;
-
+    [SerializeField]  private AudioSource kiwisAudioSource;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Kiwi"))
         {
+            kiwisAudioSource.Play();
             Destroy(collision.gameObject);
             kiwis++;
 
--- Platformer/Assets/Scripts/Player_Movement.cs
+++ Platformer/Assets/Scripts/Player_Movement.cs
@@ -18,6 +18,10 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] private float jumpForce = 14f;
     private enum MovementState {idle,running,jumping,falling }
 
+
+
+    [SerializeField] private AudioSource jumpSoundEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,7 +39,7 @@ public class Player_Movement : MonoBehaviour
 
         if(Input.GetButtonDown("Jump") && IsGrounded())
         {
-
+            jumpSoundEffect.Play();
           rb.velocity = new Vector2(rb.velocity.x,jumpForce);
         }
 
