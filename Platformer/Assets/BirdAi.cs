51d32f38e3cba0f0eb40964eeb936985be5f8963
GhaithBrunel <2157226@brunel.ac.uk>
2023-11-05 20:58:44 +0000

    added Cherry

--- Platformer/Assets/BirdAi.cs
+++ Platformer/Assets/BirdAi.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class BasicAI : MonoBehaviour
 {
-    public Transform player;  // Reference to the player object
-    public float speed = 5.0f;  // Speed at which the AI follows the player
-    public LayerMask groundLayer;  // Specify the ground layer
+    [SerializeField]
+    private Transform player;  // Reference to the player object
+    [SerializeField]
+    private float speed = 5.0f;  // Speed at which the AI follows the player
+    [SerializeField]
+    private LayerMask groundLayer;  // Specify the ground layer
+    [SerializeField]
+    private Transform triggerPoint;  // Reference to the empty GameObject where the player triggers the bird to follow
 
+    private bool isFollowing = false;
     private Rigidbody2D rb;
     private Transform aiTransform;
 
@@ -21,36 +27,51 @@ public class BasicAI : MonoBehaviour
     {
         if (player != null)
         {
-            // Calculate the direction to the player
-            Vector2 direction = player.position - aiTransform.position;
-            direction.Normalize();
-
-            // Flip the AI's sprite to face the player
-            if (direction.x < 0)
+            // If the player hits the trigger point, start following
+            if (!isFollowing && triggerPoint != null && triggerPoint.GetComponent<Collider2D>().IsTouching(player.GetComponent<Collider2D>()))
             {
-                aiTransform.localScale = new Vector3(Mathf.Abs(aiTransform.localScale.x), aiTransform.localScale.y, aiTransform.localScale.z); // Face right
-            }
-            else if (direction.x > 0)
-            {
-                aiTransform.localScale = new Vector3(-Mathf.Abs(aiTransform.localScale.x), aiTransform.localScale.y, aiTransform.localScale.z); // Face left
+                isFollowing = true;
             }
 
-            // Perform a raycast to check for obstacles (using the specified layer)
-            RaycastHit2D hit = Physics2D.Raycast(aiTransform.position, direction, 1.0f, groundLayer);
-
-            if (hit.collider != null)
+            if (isFollowing)
             {
-                // An obstacle on the specified layer is detected, stop moving
-                rb.velocity = Vector2.zero;
+                // Calculate the direction to the player
+                Vector2 direction = player.position - aiTransform.position;
+                direction.Normalize();
+
+                // Flip the AI's sprite to face the player
+                if (direction.x < 0)
+                {
+                    aiTransform.localScale = new Vector3(Mathf.Abs(aiTransform.localScale.x), aiTransform.localScale.y, aiTransform.localScale.z); // Face right
+                }
+                else if (direction.x > 0)
+                {
+                    aiTransform.localScale = new Vector3(-Mathf.Abs(aiTransform.localScale.x), aiTransform.localScale.y, aiTransform.localScale.z); // Face left
+                }
+
+                // Perform a raycast to check for obstacles (using the specified layer)
+                RaycastHit2D hit = Physics2D.Raycast(aiTransform.position, direction, 1.0f, groundLayer);
+
+                if (hit.collider != null)
+                {
+                    // An obstacle on the specified layer is detected, stop moving
+                    rb.velocity = Vector2.zero;
+                }
+                else
+                {
+                    // No obstacle detected, move the AI toward the player
+                    rb.velocity = direction * speed;
+                }
             }
             else
             {
-                // No obstacle detected, move the AI toward the player
-                rb.velocity = direction * speed;
+                // The AI is not following, so it should remain stationary
+                rb.velocity = Vector2.zero;
             }
         }
     }
 }
+
 
 
 
